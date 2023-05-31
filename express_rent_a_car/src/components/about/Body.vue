ae47a5565888697f3e0b8365a9e98cbbc7ec9fdc
matutaj <matutajorge2002@gmail.com>
2023-05-31 22:22:23 +0100

    adicionando as primeiras configurações

--- express_rent_a_car/src/components/about/Body.vue
+++ express_rent_a_car/src/components/about/Body.vue
@@ -10,7 +10,7 @@
 
                     <div class="img">
                         <img :src="img" :alt="alt">
-                        
+
                         <div class="paragraph">
                             <p>
                                 Bem-vindo a Express Rent a car, a sua solução completa para aluguer de carros!
@@ -29,12 +29,12 @@
                             <p>
                                 Se você está procurando um carro para as suas ferias, uma viagem de negócios ou
                                 qualquer outra formalidade, nosso site é a escolha certa para ajudá-lo a encontrar
-                                o carro perfeito. Com uma ampla seleção de modelos e tamanhos, temos algo para 
+                                o carro perfeito. Com uma ampla seleção de modelos e tamanhos, temos algo para
                                 atender às necessidades de todos os nossos clientes.
                             </p>
 
                             <p>
-                                Então, sinta-se à vontade para explorar nosso site e descobrir as melhores opções 
+                                Então, sinta-se à vontade para explorar nosso site e descobrir as melhores opções
                                 de aluguer de carro para você. Se você tiver alguma dúvida ou precisar de ajuda
                                 para escolher o carro certo, a nossa equipe de suporte está sempre pronta para ajudar.
                                 Obrigado por escolher Express Rent a car.
@@ -48,71 +48,72 @@
 </template>
 
 <script>
-    export default {
-        name: 'Body',
+export default {
+    name: 'Body',
 
-        data() {
-            return {
-                img: require("@/assets/img/sobre.jpeg"),
-                alt: "Carro"
-            }
+    data() {
+        return {
+            img: require("@/assets/img/sobre.jpeg"),
+            alt: "Carro"
         }
     }
+}
 </script>
 
 <style scoped>
-    .container{
-        padding: 5rem 13rem;
-        margin: 0 auto;
-        box-sizing: border-box;
-    }
-    .heading{
-        letter-spacing: 1px;
-        margin: 3.1rem 0;
-        padding: 10px 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+.container {
+    padding: 5rem 13rem;
+    margin: 0 auto;
+    box-sizing: border-box;
+}
 
-    .heading h1{
-        font-size: 1.5rem;
-        font-weight: 500;
-        background-color: #232f33;
-        color: #fff;
-        padding: 10px 20px;
-        text-transform: uppercase;
-    }
+.heading {
+    letter-spacing: 1px;
+    margin: 3.1rem 0;
+    padding: 10px 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
-    .heading span{
-        font-size: 1rem;
-        color: #222;
-        margin-bottom: 10px;
-        letter-spacing: 2px;
-        text-transform: uppercase;
-    }
+.heading h1 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    background-color: #232f33;
+    color: #fff;
+    padding: 10px 20px;
+    text-transform: uppercase;
+}
 
-    img{
-        width: 1300px;
-        margin-bottom: 2rem;
-    }
+.heading span {
+    font-size: 1rem;
+    color: #222;
+    margin-bottom: 10px;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+}
 
-    .paragraph{
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
-        box-sizing: border-box;
-        text-align: justify;
-    }
+img {
+    width: 1300px;
+    margin-bottom: 2rem;
+}
 
-    .paragraph p{
-        width: 500px;
-        margin: 1rem 2rem;
-        padding: 0px;
-        font-size: 18px
-    }
+.paragraph {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    text-align: justify;
+}
+
+.paragraph p {
+    width: 500px;
+    margin: 1rem 2rem;
+    padding: 0px;
+    font-size: 18px
+}
 </style>
