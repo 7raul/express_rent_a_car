1c03f86fea5d4c82bb54ca0a957128869c03a93d
7raul <raulmigueleb7@gmail.com>
2023-06-27 19:32:58 +0100

    Header feesdback commit

---
+++ express_rent_a_car/src/components/FeedbackClientes/FeedbackClientes.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="feedback-container">
+            <header>
+                <div class="form-container">
+                    <div class="content-container">
+                        <div class="feedback-content">
+                            <h3>Feedback</h3>
+
+                            <p>Opine e leia opiniões dos usuários</p>
+                        </div>
+
+                        <div class="feedback">
+                            <textarea placeholder="Escreva sua opinião..."></textarea>
+                            <button>Publicar</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FeedbackCliente'
+    }
+</script>
+
+<style scoped>
+    @import '@/Styles/FeedbackClientes/FeedbackClientes.sass';
+</style>
--- express_rent_a_car/src/views/FeedbackView.vue
+++ express_rent_a_car/src/views/FeedbackView.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
+        <Header />
+        <FeedbackCliente />
         <Footer />
     </div>
 </template>
 
 <script>
-
+    import Header from '@/components/home/Header.vue'
+    import FeedbackCliente from '@/components/FeedbackClientes/FeedbackClientes.vue'
     import Footer from '@/components/home/Footer.vue'
-
     export default{
         name: 'FeedbackView',
 
         components: {
+            Header,
+            FeedbackCliente,
             Footer
         }
     }
 </script>
 
 <style scoped>
-    *{
-        background-color: #ecf7fa;
-        color: #232f33;
-    }
 </style>
