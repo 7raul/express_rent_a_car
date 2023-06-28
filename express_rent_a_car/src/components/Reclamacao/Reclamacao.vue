d29c0765f4f61770481cad9cc3c43f2c230f3a1a
7raul <raulmigueleb7@gmail.com>
2023-06-28 20:06:20 +0100

    Reclamacao done

---
+++ express_rent_a_car/src/components/Reclamacao/Reclamacao.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="reclamacao-container">
+            <div class="form-container">
+                <div class="content-container">
+                    <div class="reclamacao-content">
+                        <h3>Reclamacões</h3>
+
+                        <p>Apresente aqui suas reclamações</p>
+                        <p>A nossa equipe terá muito praser de atenter</p>
+                        <p>Obrigado!</p>
+                    </div>
+
+                    <div class="reclamacao">
+                        <form>
+                            <input type="text" placeholder="Escreva o assunto...">
+                            <textarea placeholder="Escreva sua reclamação..."></textarea>
+                            <button>Publicar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Reclamacao'
+    }
+</script>
+
+<style scoped>
+    @import '@/Styles/Reclamacao/Reclamacao.sass'
+</style>
--- express_rent_a_car/src/views/ReclamacaoView.vue
+++ express_rent_a_car/src/views/ReclamacaoView.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
+        <Header />
+        <Reclamacao />
         <Footer />
     </div>
 </template>
 
 <script>
+    import Header from '@/components/home/Header.vue'
+    import Reclamacao from '@/components/Reclamacao/Reclamacao.vue'
     import Footer from '@/components/home/Footer.vue'
     export default{
         name: 'ReclamacaoView',
 
         components: {
+            Header,
+            Reclamacao,
             Footer
         }
     }
 </script>
 
 <style scoped>
-    *{
-    width: 100%;
-    background-color: #ecf7fa;
-    color: #232f33;
-  }
+
 </style>
