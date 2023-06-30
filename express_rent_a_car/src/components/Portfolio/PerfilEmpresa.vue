5e58dc8c295a4d0f7798f73e372a77b289b1c232
7raul <raulmigueleb7@gmail.com>
2023-07-01 00:00:14 +0100

    Update PerfilEmpresa.vue

--- express_rent_a_car/src/components/Portfolio/PerfilEmpresa.vue
+++ express_rent_a_car/src/components/Portfolio/PerfilEmpresa.vue
@@ -181,6 +181,133 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="carro">
+                                <div class="img">
+                                    <img :src="carImg" :alt="alt">
+                                </div>
+
+                                <div class="detalhes">
+                                    <div class="title">
+                                        <h3>Fiat 500</h3>
+                                    </div>
+
+                                    <div class="reviews">
+                                        <div>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="far fa-star"></i>
+                                        </div>
+                                        <p>4.4 <span>(10,482 reviews)</span></p>
+                                    </div>
+
+                                    <div class="price-container">
+                                        <div class="dia">
+                                            <p>Por dia:</p>
+                                        </div>
+                                        <div class="price">
+                                            <p>27.000kz</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="reservar">
+                                        <div class="provincia">
+                                            <p>Luanda</p>
+                                        </div>
+
+                                        <div class="btn-reservar">
+                                            <button>Reservar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carro">
+                                <div class="img">
+                                    <img :src="carImg" :alt="alt">
+                                </div>
+
+                                <div class="detalhes">
+                                    <div class="title">
+                                        <h3>Fiat 500</h3>
+                                    </div>
+
+                                    <div class="reviews">
+                                        <div>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="far fa-star"></i>
+                                        </div>
+                                        <p>4.4 <span>(10,482 reviews)</span></p>
+                                    </div>
+
+                                    <div class="price-container">
+                                        <div class="dia">
+                                            <p>Por dia:</p>
+                                        </div>
+                                        <div class="price">
+                                            <p>27.000kz</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="reservar">
+                                        <div class="provincia">
+                                            <p>Luanda</p>
+                                        </div>
+
+                                        <div class="btn-reservar">
+                                            <button>Reservar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carro">
+                                <div class="img">
+                                    <img :src="carImg" :alt="alt">
+                                </div>
+
+                                <div class="detalhes">
+                                    <div class="title">
+                                        <h3>Fiat 500</h3>
+                                    </div>
+
+                                    <div class="reviews">
+                                        <div>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="far fa-star"></i>
+                                        </div>
+                                        <p>4.4 <span>(10,482 reviews)</span></p>
+                                    </div>
+
+                                    <div class="price-container">
+                                        <div class="dia">
+                                            <p>Por dia:</p>
+                                        </div>
+                                        <div class="price">
+                                            <p>27.000kz</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="reservar">
+                                        <div class="provincia">
+                                            <p>Luanda</p>
+                                        </div>
+
+                                        <div class="btn-reservar">
+                                            <button>Reservar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
                         </div> 
                     </div>
                 </div>
