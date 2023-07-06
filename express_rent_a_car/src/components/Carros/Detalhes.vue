1870fe84309504d651662beb0b388a8cb5a58c5f
7raul <raulmigueleb7@gmail.com>
2023-07-07 00:27:33 +0100

    form

--- express_rent_a_car/src/components/Carros/Detalhes.vue
+++ express_rent_a_car/src/components/Carros/Detalhes.vue
@@ -21,93 +21,104 @@
                         <p>4.4 <span>(10,482 reviews)</span></p>
                     </div>
                 </div>
-                <div class="detalhes">
-                    <div class="img-car">
-                        <img :src="carImg" >
-                    </div>
+                <div class="detalhes-carro">
+                    <div class="detalhes">
+                        <div class="img-car">
+                            <img :src="carImg" >
+                        </div>
 
-                    <div class="info">
-                        <div class="info1">
-                            <div class="dados">
-                                <p>Passageiros:</p>
-                                <p class="color">5</p>
+                        <div class="info">
+                            <div class="info1">
+                                <div class="dados">
+                                    <p>Passageiros:</p>
+                                    <p class="color">5</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Direção:</p>
+                                    <p class="color">Hidráulica</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Vidros eléctricos:</p>
+                                    <p class="color">Sim</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Airbag:</p>
+                                    <p class="color">Sim</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Portas:</p>
+                                    <p class="color">4</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Malas:</p>
+                                    <p class="color">1</p>
+                                </div>
                             </div>
-                            <div class="dados">
-                                <p>Direção:</p>
-                                <p class="color">Hidráulica</p>
-                            </div>
-                            <div class="dados">
-                                <p>Vidros eléctricos:</p>
-                                <p class="color">Sim</p>
-                            </div>
-                            <div class="dados">
-                                <p>Airbag:</p>
-                                <p class="color">Sim</p>
-                            </div>
-                            <div class="dados">
-                                <p>Portas:</p>
-                                <p class="color">4</p>
-                            </div>
-                            <div class="dados">
-                                <p>Malas:</p>
-                                <p class="color">1</p>
+
+                            <div class="info1">
+                                <div class="dados">
+                                    <p>Ar condicionado:</p>
+                                    <p class="color">Sim</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Cambio:</p>
+                                    <p class="color">Autómatico</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Trava Electrica:</p>
+                                    <p class="color">Sim</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Freiod ABS:</p>
+                                    <p class="color">SIm</p>
+                                </div>
+                                <div class="dados">
+                                    <p>Áudio:</p>
+                                    <p class="color">Rádio/USB</p>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="info1">
-                            <div class="dados">
-                                <p>Ar condicionado:</p>
-                                <p class="color">Sim</p>
-                            </div>
-                            <div class="dados">
-                                <p>Cambio:</p>
-                                <p class="color">Autómatico</p>
-                            </div>
-                            <div class="dados">
-                                <p>Trava Electrica:</p>
-                                <p class="color">Sim</p>
-                            </div>
-                            <div class="dados">
-                                <p>Freiod ABS:</p>
-                                <p class="color">SIm</p>
-                            </div>
-                            <div class="dados">
-                                <p>Áudio:</p>
-                                <p class="color">Rádio/USB</p>
-                            </div>
+                    </div>
+                    
+                    <div class="pagamento-zone">
+                        <h4>Selecione sua opção de entrega</h4>
+                        <div class="att">
+                            <form action="#">
+                                <div class="hotel">
+                                    <input type="radio" required>
+                                    <span>Hotel</span>
+                                </div>
+                                <div class="aeroporto">
+                                    <input type="radio" required>
+                                    <span>Aeroporto</span>
+                                </div>
+                                <div class="balcao">
+                                    <input type="radio" required>
+                                    <span>Balcão</span>
+                                </div>
+                            </form>
+
+                            <button type="submit">Continuar ></button>
                         </div>
                     </div>
                 </div>
 
-                <div class="pagamento-zone">
-                    <h4>Selecione sua opção de entrega</h4>
-                    <div class="att">
-                        <form action="#">
-                            <div class="hotel">
-                                <input type="radio" required>
-                                <span>Hotel</span>
-                            </div>
-                            <div class="aeroporto">
-                                <input type="radio" required>
-                                <span>Aeroporto</span>
-                            </div>
-                            <div class="balcao">
-                                <input type="radio" required>
-                                <span>Balcão</span>
-                            </div>
-                        </form>
-
-                        <button type="submit">Continuar ></button>
-                    </div>
-                </div>
+                <!--Reservado para componentes-->
+                <FormHotel />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import FormHotel from '@/components/Carros/FormHotel.vue'
     export default {
         name: 'Detalhes',
+
+        components: {
+            FormHotel,
+        },
 
         data() {
             return{
---
+++ express_rent_a_car/src/components/Carros/FormHotel.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <div class="form-hotel">
+            <form action="#">
+                <div class="entrega-devolucao">
+                    <div class="entrega">
+                        <div class="local">
+                            <label for="#">Local de entrega</label>
+                            <input type="text" required placeholder="Nome do hotel">
+                        </div>
+
+                        <div class="data-time">
+                            <div class="data">
+                                <label for="#">Data de entrega</label>
+                                <input type="date">
+                            </div>
+
+                            <div class="hora">
+                                <label for="#">hora de entrega</label>
+                                <input type="time">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="devolucao">
+
+                        <div class="opcao-devolucao">
+                            <div class="local-devolucao">
+                                <input type="checkbox">
+                                <span>Devolver no mesmo local</span>
+                            </div>
+
+                            <div class="tempo-devolucao">
+                                <input type="checkbox">
+                                <span>Devolver na mesma hora/dia</span>
+                            </div>
+                        </div>
+
+                        <div class="local">
+                            <label for="#">Local de devolução</label>
+                            <input type="text" required placeholder="Nome do hotel">
+                        </div>
+
+                        <div class="data-time">
+                            <div class="data">
+                                <label for="#">Data de devolução</label>
+                                <input type="date">
+                            </div>
+
+                            <div class="hora">
+                                <label for="#">hora de devolução</label>
+                                <input type="time">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="dados-condutor">
+                    <div class="input-condutor">
+                        <label for="#">Nome</label>
+                        <input type="text" required placeholder="Nome do condutor">
+                    </div>
+
+                    <div class="input-condutor">
+                        <label for="#">E-mail</label>
+                        <input type="e-mail" required placeholder="E-mail do condutor">
+                    </div>
+
+                    <div class="numbers">
+                        <div class="phone">
+                            <label for="#">Telefone</label>
+                            <input type="number" required placeholder="Numero de telefone">
+                        </div>
+
+                        <div class="bi">
+                            <label for="#">B.I</label>
+                            <input type="text" required placeholder="Numero do B.I">
+                        </div>
+                    </div>
+                    
+                    <div class="hote-number">
+                        <label for="#">Quarto</label>
+                        <input type="text" required placeholder="Numero do quarto">
+                    </div>
+                </div>
+            </form>
+
+            <div class="btn">
+                <div class="checkbox">
+                    <input type="checkbox">
+                    <span>Condutor com a idade entre 20 e 60 anos</span>
+                </div>
+
+                <button type="submit">Continuar ></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FormHotel'
+    }
+</script>
+
+<style scoped>
+    @import '@/Styles/Carros/FormHotel.sass';
+</style>
